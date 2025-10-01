3584e9ef49e7920c5b2268aaa6d5b4b05f3a96f5
Freddy León <90221188+Fredleo3@users.noreply.github.com>
2025-09-30 20:05:15 -0500

    feat: agregar movimiento de tareas entre columnas y refactorizar filtro con delegación de listeners -m Este commit completa el MVP propuesto: creación, persistencia, filtrado y movimiento de tareas

--- app.js
+++ app.js
@@ -6,50 +6,109 @@ const scheduledTaskList = document.getElementById("task-list-scheduled");
 const inProgressTaskList = document.getElementById("task-list-in-progress");
 const doneTaskList = document.getElementById("task-list-done");
 
-const filterBtn = document.getElementById("task-filter");
-const filterOptions = document.querySelectorAll(".task-filter__option");
 const filterGroup = document.getElementById("task-filter-group");
-
 
 // Inicio de la aplicación
 
 document.addEventListener("DOMContentLoaded", () => {
   renderTasks(getAllTasks());
-  loadFilter(getFilter())
+  loadFilter(getFilter());
+});
+
+document.addEventListener("click", (e) => {
+  const filterBtn = e.target.closest("#task-filter");
+  const filterOption = e.target.closest(".task-filter__option");
+
+  const actionBtn = e.target.closest(".task-action");
+  const moveActions = e.target.closest(".task-action__option");
+
+  // Abriendo el filtro de tareas en móvil
+
+  if (filterBtn) {
+    if (isOpen(filterGroup)) {
+      filterGroup.classList.remove("open");
+      filterGroup.classList.add("hidden");
+    } else {
+      closeAllActions();
+      filterGroup.classList.remove("hidden");
+      filterGroup.classList.add("open");
+    }
+  }
+  // Seleccionando una columna
+  else if (filterOption) {
+    closeAllActions();
+    filter(filterOption);
+    saveFilter(filterOption.value);
+  }
+  //  Abriendo el menú de opciones
+  else if (actionBtn) {
+    const column = e.target.closest("section");
+    const task = e.target.closest("li");
+    const optionMenu = task.querySelector(".task-action__group");
+    if (isOpen(optionMenu)) {
+      optionMenu.classList.remove("open");
+      optionMenu.classList.add("hidden");
+      return;
+    } else {
+      closeAllActions();
+      optionMenu.classList.add("open");
+      optionMenu.classList.remove("hidden");
+      updateOptions(column, optionMenu);
+      return;
+    }
+  }
+
+  // clickeando una opción para mover la tarea
+  else if (moveActions) {
+    closeAllActions();
+    const columnTarget = moveActions.value;
+    const task = e.target.closest("li");
+    moveToCol(columnTarget, task);
+
+    return;
+  } else {
+    closeAllActions();
+    return;
+  }
 });
 
 // Nueva tarea ______________________________________________________________________
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  newTask(inputTask.value);
+  newTask(inputTask.value.trim());
 });
 
 const newTask = (newText) => {
-  const task = { text: newText, state: "new" };
+  const task = { taskId: idGenerator(), text: newText, state: "new" };
   saveTask(task);
   renderTasks([task]);
   inputTask.value = "";
 };
+
+const idGenerator = () => Date.now();
 
 // Renderizar tareas ________________________________________________________________
 
 const renderTasks = (taskList) => {
   for (let i = 0; i < taskList.length; i++) {
     const taskCode = `
-        <li class="task">
+        <li class="task" data-task-id='${taskList[i].taskId}'>
               <span class="task-text">
                 ${taskList[i].text}</span>              
               <section class="task-action__group hidden">
                 <h5 class="task-action__title">Mover a tareas</h5>
+                <button class="task-action__option" value="new">
+                  Nuevas 1
+                </button>
                 <button class="task-action__option" value="scheduled">
-                  Programadas
+                  Programadas 2
                 </button>
                 <button class="task-action__option" value="in-progress">
-                  Iniciadas
+                  Iniciadas 3
                 </button>
                 <button class="task-action__option" value="done">
-                  Finalizadas
+                  Finalizadas 4
                 </button>
               </section>
 
@@ -71,29 +130,13 @@ const renderTasks = (taskList) => {
 
 // Filtro _____________________________________________________________
 
-filterBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  filterGroup.classList.toggle("hidden");
-});
-
-filterOptions.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    filterGroup.classList.toggle("hidden");
-    const selected = e.target.closest("button");
-    filter(selected)
-    saveFilter(selected.value)
-  });
-});
-
-const filter = (selected) => {      
-    const column = getColumn(selected.value);
-    hideColumn();
-    showColumn(column);
-    updatemenu(selected);
-    changeSelectName(selected.value);
-    
-}
+const filter = (selected) => {
+  const columnName = getColumn(selected.value);
+  hideColumn();
+  showColumn(columnName);
+  updateMenu(selected);
+  changeSelectName(selected.value);
+};
 
 const getColumn = (selected) => {
   let columnId = "";
@@ -105,15 +148,16 @@ const getColumn = (selected) => {
   return document.getElementById(columnId).closest("section");
 };
 
-const changeSelectName = (selected) => {
+const changeSelectName = (value) => {
   let name = "";
 
-  selected === "new" ? (name = " Nuevas") : "";
-  selected === "scheduled" ? (name = " Programadas") : "";
-  selected === "in-progress" ? (name = " Iniciadas") : "";
-  selected === "done" ? (name = " Finalizadas") : "";
+  value === "new" ? (name = " Nuevas") : "";
+  value === "scheduled" ? (name = " Programadas") : "";
+  value === "in-progress" ? (name = " Iniciadas") : "";
+  value === "done" ? (name = " Finalizadas") : "";
 
-  filterBtn.children[0].children[0].innerHTML = name;
+  const columnName = document.querySelector(".task-filter__column-name");
+  columnName.innerHTML = name;
 };
 
 const hideColumn = () => {
@@ -127,32 +171,69 @@ const showColumn = (column) => {
   column.classList.remove("hidden");
 };
 
-const updatemenu = (selected) => {
+const updateMenu = (selected) => {
   const option = document.querySelector(".task-filter__option.hidden");
   option.classList.remove("hidden");
   selected.classList.add("hidden");
 };
 
 const loadFilter = (filterValue) => {
-  const selected = document.querySelector(`[value=${filterValue}]`)
+  const selected = document.querySelector(`[value=${filterValue}]`);
   const column = getColumn(selected.value);
   hideColumn();
   showColumn(column);
-  updatemenu(selected);
+  updateMenu(selected);
   changeSelectName(selected.value);
-}
+};
+
+// Acciones _________________________________________________________________________________
+
+const isOpen = (element) => {
+  return element.classList.contains("open");
+};
+
+const closeAllActions = () => {
+  const openedMenu = document.querySelectorAll(".open");
+  if (openedMenu.length > 0) {
+    for (let i = 0; i < openedMenu.length; i++) {
+      openedMenu[i].classList.add("hidden");
+      openedMenu[i].classList.remove("open");
+    }
+  }
+};
+
+const moveToCol = (columnTarget, task) => {
+  columnTarget === "new" ? newTaskList.appendChild(task) : 0;
+  columnTarget === "scheduled" ? scheduledTaskList.appendChild(task) : 0;
+  columnTarget === "in-progress" ? inProgressTaskList.appendChild(task) : 0;
+  columnTarget === "done" ? doneTaskList.appendChild(task) : 0;
+  const taskId = task.dataset.taskId;
+  upadateTask(taskId, "state", columnTarget);
+};
+
+const updateOptions = (column, optionGroup) => {
+  const allOptions = optionGroup.querySelectorAll(".task-action__option");
+
+  allOptions.forEach((option) => {
+    if (option.value != column.dataset.value) {
+      option.classList.remove("hidden");
+    } else {
+      option.classList.add("hidden");
+    }
+  });
+};
 
 // LocalStorage _____________________________________________________________________________
 
 // --- LocalStorage-Filtro
 
 const saveFilter = (value) => {
-  localStorage.setItem('filter', value)
-}
+  localStorage.setItem("filter", value);
+};
 
-const getFilter = () => { 
-  return localStorage.getItem('filter')
-}
+const getFilter = () => {
+  return localStorage.getItem("filter");
+};
 
 // --- LocalStorage-Tareas
 
@@ -166,5 +247,14 @@ const getAllTasks = () => {
   return JSON.parse(localStorage.getItem("allTasks")) || [];
 };
 
+const upadateTask = (id, key, value) => {
+  let allTasks = getAllTasks();
 
+  for (let i = 0; i < allTasks.length; i++) {
+    if (allTasks[i].taskId === +id) {
+      allTasks[i][key] = value;
+    }
+  }
 
+  localStorage.setItem("allTasks", JSON.stringify(allTasks));
+};
